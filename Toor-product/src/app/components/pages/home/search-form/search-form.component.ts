039e797d9adb17debb14dcccb025b2fbc39dc879
Wissal <wissalghiryani07@gmail.com>
2025-03-21 12:28:20 +0100

    Message expliquant les modifications

--- Toor-product/src/app/components/pages/home/search-form/search-form.component.ts
+++ Toor-product/src/app/components/pages/home/search-form/search-form.component.ts
@@ -1,7 +1,8 @@
+// search-form.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { VoyageService, TypeVoyage } from 'src/app/components/pages/tableau-de-bord/services/voyage.service';
-
+import { AllVoyageService, TypeVoyage, Voyage, Itineraire } from 'src/app/components/pages/home/all-voyage/service/all-voyage.service';
+import { VoyageService } from 'src/app/components/pages/tableau-de-bord/services/voyage.service';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
@@ -18,11 +19,11 @@ export class SearchFormComponent implements OnInit {
   // Liste des types de voyage
   tripTypes: TypeVoyage[] = [];
 
-  constructor(private router: Router, private voyageService: VoyageService) {}
+  constructor(private router: Router, private AllVoyageService: AllVoyageService,private VoyageService: VoyageService ) {}
 
   ngOnInit(): void {
     // Récupérer les types de voyage depuis la base de données
-    this.voyageService.getTypesVoyage().subscribe({
+    this.VoyageService.getTypesVoyage().subscribe({
       next: (types) => {
         this.tripTypes = types;
       },
@@ -34,9 +35,34 @@ export class SearchFormComponent implements OnInit {
 
   // Méthode appelée lors de la soumission du formulaire
   onSearch() {
-    // Rediriger vers la page des résultats avec les critères de recherche
-    this.router.navigate(['/voyages'], {
-      queryParams: this.searchCriteria,
-    });
+    if (this.searchCriteria.tripType) {
+      // Récupérer les voyages par type
+      this.AllVoyageService.getVoyagesByType(this.searchCriteria.tripType).subscribe({
+        next: (voyages) => {
+          const itineraires: { [voyageId: number]: Itineraire[] } = {};
+
+          // Pour chaque voyage, récupérer les itinéraires
+          voyages.forEach((voyage) => {
+            this.AllVoyageService.getItinerairesByVoyageId(voyage.idVoyage).subscribe({
+              next: (itinerairesVoyage) => {
+                itineraires[voyage.idVoyage] = itinerairesVoyage;
+              },
+              error: (err) => {
+                console.error(`Erreur lors de la récupération des itinéraires pour le voyage ${voyage.idVoyage} :`, err);
+              },
+            });
+          });
+
+          // Stocker les résultats dans le service
+          this.AllVoyageService.setSearchResults(voyages, itineraires);
+
+          // Naviguer vers la page all-voyage
+          this.router.navigate(['/all-voyage']);
+        },
+        error: (err) => {
+          console.error('Erreur lors de la récupération des voyages :', err);
+        },
+      });
+    }
   }
 }
